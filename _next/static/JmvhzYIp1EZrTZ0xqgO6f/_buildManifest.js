9f8e37137a3936b0115d4977bdb614c2acc0514d
danrr <danristea@protonmail.com>
2021-10-19 23:02:34 +0000

    Update website

---
+++ _next/static/JmvhzYIp1EZrTZ0xqgO6f/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4c284bad7feebcfc271c.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],"/blog":["static/chunks/pages/blog-456be729c1101fc1c075.js"],sortedPages:["/","/_app","/_error","/blog"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
