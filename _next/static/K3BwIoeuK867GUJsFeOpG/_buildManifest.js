a968fd76eb9fe074c2376bb80fed776a8bafc9e2
danrr <danristea@protonmail.com>
2023-10-25 22:48:01 +0000

    Update website

--- _next/static/K3BwIoeuK867GUJsFeOpG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-6576690cce3b0a46.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
